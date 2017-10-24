f7ebb60c2f35293f2b830b55de890d7d0f77d29f
Jasper Moelker <j.b.moelker@activeids.nl>
2017-10-24 22:48:58 +0200

    Share core between app & storybook

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,9 +1,5 @@
 import { configure } from '@storybook/vue'
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-
-Vue.use(Vuetify)
-require('vuetify/dist/vuetify.min.css')
+import '../../src/core'
 
 const req = require.context(`../../src/components`, true, /\.stories\.js$/)
 const loadStories = () => req.keys().map(path => req(path))
